0661c075af2abc945c754d8ada72ed3b9f28628b
Mikhail <hero_simplex73@mail.ru>
2023-05-03 20:01:51 +0400

    fix solution.rb fix mutation

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,18 +5,16 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(array)
-        newarray = []
-        array.map do |film|
+        newarray = array.map do |film|
           country = film['country'] || ''
           country_count = country.split(',').size
-          newarray << film['rating_kinopoisk'].to_f if film['rating_kinopoisk'].to_f.positive? && country_count > 1
-        end
+          film['rating_kinopoisk'].to_f if film['rating_kinopoisk'].to_f.positive? && country_count > 1
+        end.compact
         newarray.reduce { |sum, num| sum + num } / newarray.size
       end
 
       def chars_count(films, threshold)
-        film_names = []
-        films.map { |film| film_names << film['name'] if film['rating_kinopoisk'].to_f >= threshold }
+        film_names = films.map { |film| film['name'] if film['rating_kinopoisk'].to_f >= threshold }.compact
         film_names.reduce(0) { |sum_of_char, film_name| sum_of_char + film_name.gsub(/и/).count }
       end
     end
